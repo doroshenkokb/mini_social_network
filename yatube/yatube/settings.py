88a2bd077222dc745a11eedc8c3ee96ae8585585
Кирилл Дорошенко <doroshenko_kirill@bk.ru>
2023-04-20 12:23:46 +0300

    Статика

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -43,6 +43,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 
+#STATIC_URL = '/static/'
+
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MIDDLEWARE = [
